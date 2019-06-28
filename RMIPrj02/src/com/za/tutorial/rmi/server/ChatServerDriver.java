983c563625e3b1f3a80cab8f60427bcd8ecdef62
ViniciusGuilhermeDeOliveira123 <47893211+ViniciusGuilhermeDeOliveira123@users.noreply.github.com>
2019-06-28 00:29:41 -0300

    Add files via upload
    1̣ - rmiregistry
    2 - java ChatServerDriver
    3 - java ClientServerDriver

---
+++ RMIPrj02/src/com/za/tutorial/rmi/server/ChatServerDriver.java
@@ -0,0 +1,9 @@
+package com.za.tutorial.rmi.server;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+public class ChatServerDriver {
+	public static void main(String[]args) throws RemoteException, MalformedURLException {
+		Naming.rebind("RMIChatServer", new ChatServer());
+	}
+}
